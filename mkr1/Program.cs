6c40cd0f165e256a72638612bc14fd6d3b9b56d3
BalkoYana <126760577+BalkoYana@users.noreply.github.com>
2023-10-18 11:07:30 +0300

    Update Program.cs

--- mkr1/Program.cs
+++ mkr1/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Xml.Serialization;
 using System.Xml;
 
 namespace mkr1
