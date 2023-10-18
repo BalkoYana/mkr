c59b2b6a78ea7950a5bf14b118606b7717f054a1
BalkoYana <126760577+BalkoYana@users.noreply.github.com>
2023-10-18 11:00:13 +0300

    Add files via upload

---
+++ mkr1/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mkr1
+{
+    public class Payment
+    {
+        public string Surname { get; set; }
+        public string Type { get; set; }
+        public double Paid { get; set; }
+        public DateTime Paymentdate { get; set; }
+    }
+}
---
+++ mkr1/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mkr1
+{
+    public class Service
+    {
+        
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public double Price { get; set; }
+        
+    }
+}
